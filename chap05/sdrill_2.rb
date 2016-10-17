76ca81da8526d526b0ada211983d4e778904f73a
Asuka Ochi <asukaochi@gmail.com>
2016-10-17 14:05:25 +0000

    Chap05

---
+++ chap05/sdrill_2.rb
@@ -0,0 +1,5 @@
+my_height = 74 # inches
+my_weight = 180 # lbs
+
+puts "He's #{my_height} inches (#{my_height * 2.54} centimeters) tall."
+puts "He's #{my_weight} pounds (#{my_weight * 0.453592} kilograms) heavy."
