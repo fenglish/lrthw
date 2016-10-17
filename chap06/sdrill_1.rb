c3f9d3615fc22acd24f9baa6ed0610fc69a9a930
Asuka Ochi <asukaochi@gmail.com>
2016-10-17 14:59:57 +0000

    Chap06

---
+++ chap06/sdrill_1.rb
@@ -0,0 +1,34 @@
+# create a variable and set 10
+types_of_people = 10
+# create a variable and set a string
+x = "There are #{types_of_people} types of people."
+# create a variable and set a string
+binary = "binary"
+# create a variable and set a string
+do_not = "don't"
+# create a variable and set a string include two variables above
+y = "Those who know #{binary} and those who #{do_not}."
+
+# print out the variables
+puts x
+puts y
+
+# print out strings include the variables x and y
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+
+# create a variable and set boolean "false"
+hilarious = false
+# create a variable and set a string include the variable above
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+# print out the variables
+puts joke_evaluation
+
+# create a variable and set a string
+w = "This is the left side of..."
+# create a variable and set a string
+e = "a string with a right side."
+
+# print out the variables above. conect two variables using +
+puts w + e
