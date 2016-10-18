c161937b502c3b657fa2f52f5b6e3dc5908f9514
Asuka Ochi <asukaochi@gmail.com>
2016-10-18 10:58:29 +0000

    Chap07

---
+++ chap07/sdrill_1.rb
@@ -0,0 +1,25 @@
+# print out strings
+puts "Mary had a little lamb."
+puts "Its fleece was white as #{'snow'}."
+puts "And everywhere that Mary went."
+# print out Strngs . 10 times using *
+puts "." * 10  # what'd that do?
+
+# set a string each variable
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that print vs. puts on this line what's it do?
+# connect variables and print out a word
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
