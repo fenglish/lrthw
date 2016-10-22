82ef25896dea1e1913e72b5773efc3af544f428c
Asuka Ochi <asukaochi@gmail.com>
2016-10-22 16:32:01 +0000

    Chap34

---
+++ chap34/ex34.rb
@@ -0,0 +1,18 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# The animal at 1. 
+p animals[1] == 'ruby'
+# The third (3rd) animal. => peacock
+p animals[2] == 'peacock'
+# The first (1st) animal. => bear
+p animals[0] == 'bear'
+# The animal at 3.
+p animals[3] == 'kangaroo'
+# The fifth (5th) animal.
+p animals[5] == 'platypus'
+# The animal at 2.
+p animals[2] == 'peacock'
+# The sixth (6th) animal.
+p animals[5] == 'platypus'
+# The animal at 4.
+p animals[4] == 'whale'
